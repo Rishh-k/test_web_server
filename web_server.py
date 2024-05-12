4577c0c7647cb3bb284dff0a19c559b5b2a18659
Rishabh Khandelwal <rishh.k24@gmail.com>
2024-05-12 18:12:13 +0530

    initial commit

---
+++ web_server.py
@@ -0,0 +1,19 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def helloworld():
+    return "Hello World!"
+
+@app.route("/myinfo")
+def myinfo():
+    info = "Dev Name: Rishabh Khandelwal"
+    return info
+
+@app.route("/submission")
+def submit():
+    info = "This is my final submission"
+    return info
+
+if __name__ == "__main__":
+    app.run()
